1222f07fd81471d772bb3d8432f42325986e1d6f
kaitur <artur.kaidanskyi@gmail.com>
2021-07-20 02:57:35 +0300

    Task 2 Sum Of Integers

---
+++ sumOfIntegers.js
@@ -0,0 +1,6 @@
+function sumIOfntegers(n, m) {
+    let sum = 0;
+    for (let i = n; i <= m; i++) sum += i;
+    return sum;
+}
+console.log(sumIOfntegers(3, 8)); // sumIntegers("от", "до") - диапазон.
